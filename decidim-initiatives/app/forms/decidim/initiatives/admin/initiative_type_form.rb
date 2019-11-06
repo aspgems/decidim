f86a4271297261a381dd0d271762b8444f55f8f2
Oriol Gual <oriol@codegram.com>
2019-11-06 16:31:27 +0100

    Add settings to only allow global scope initiatives and child scope signature threshold

--- decidim-initiatives/app/forms/decidim/initiatives/admin/initiative_type_form.rb
+++ decidim-initiatives/app/forms/decidim/initiatives/admin/initiative_type_form.rb
@@ -16,6 +16,8 @@ module Decidim
         attribute :undo_online_signatures_enabled, Boolean
         attribute :attachments_enabled, Boolean
         attribute :custom_signature_end_date_enabled, Boolean
+        attribute :child_scope_threshold_enabled, Boolean
+        attribute :only_global_scope_enabled, Boolean
         attribute :promoting_committee_enabled, Boolean
         attribute :minimum_committee_members, Integer
         attribute :collect_user_extra_fields, Boolean
